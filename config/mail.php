d60af2d0727a34a228ecec2829fd23c49d884536
Ribetan Nanjwan <57963449+mnanjwan@users.noreply.github.com>
2025-12-20 21:57:53 +0100

    mai

--- config/mail.php
+++ config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@ncsportal.com'),
+        'name' => env('MAIL_FROM_NAME', 'NCS Employee Portal'),
     ],
 
 ];
