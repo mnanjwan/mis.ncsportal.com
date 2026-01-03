be9da5b460a9339e6b0a6ed27d4aeeeee966f6e6
Ribetan Nanjwan <57963449+mnanjwan@users.noreply.github.com>
2026-01-04 00:30:42 +0100

    upreates

--- app/Mail/NewRecruitCreatedMail.php
+++ app/Mail/NewRecruitCreatedMail.php
@@ -5,54 +5,35 @@ namespace App\Mail;
 use App\Models\Officer;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class NewRecruitCreatedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $recruit;
+    public $appUrl;
+
     /**
      * Create a new message instance.
      */
-    public function __construct(
-        public Officer $recruit
-    ) {
+    public function __construct(Officer $recruit)
+    {
+        $this->recruit = $recruit;
+        $this->appUrl = config('app.url');
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Welcome to NCS - Your Recruit Profile Has Been Created',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'emails.new-recruit-created',
-            with: [
-                'recruit' => $this->recruit,
-                'appUrl' => config('app.url'),
-            ],
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Welcome to NCS - Your Recruit Profile Has Been Created')
+                    ->view('emails.new-recruit-created')
+                    ->with([
+                        'recruit' => $this->recruit,
+                        'appUrl' => $this->appUrl,
+                    ]);
     }
 }
 
